6952e2ec40b709f1c2d1d5aa63b0b68dc943d836
RidhaMuneer <ridhamuner@gmail.com>
2024-10-25 12:03:05 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import alias from '@rollup/plugin-alias';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname in ES module context
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), alias()],
